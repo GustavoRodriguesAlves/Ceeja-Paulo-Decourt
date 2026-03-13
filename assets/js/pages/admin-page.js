31f09f2e76b9124e40a38707a56302b857dfaa74
Gustavo Rodrigues Alves <eugustavorodriguesalves@gmail.com>
2026-03-13 11:26:28 -0300

    Deploy latest admin script update

--- assets/js/pages/admin-page.js
+++ assets/js/pages/admin-page.js
@@ -1,4 +1,4 @@
-﻿import {
+import {
   TEST_LOGIN_EMAIL,
   clearAdminAuth,
   getAdminSessionEmail,
@@ -110,7 +110,7 @@ const formatDate = (value) => {
 
 function setStatus(message) {
   if (adminStatus) {
-    adminStatus.innerHTML = <svg class="h-5 w-5 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>  + message;
+    adminStatus.innerHTML = `<svg class="h-5 w-5 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg> ` + message;
   }
 }
 
